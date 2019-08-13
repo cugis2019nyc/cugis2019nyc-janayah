f3019c5fbbae38bb7196eb4c2f0e72ecd41b85b2
janayah <54039981+janayah@users.noreply.github.com>
2019-08-13 14:58:52 -0400

    Add files via upload

--- First day of python.py
+++ First day of python.py
@@ -4,4 +4,37 @@ Spyder Editor
 
 This is a temporary script file.
 """
+print("hello world") #greeting
+print ("228") #number
+print("my name is janayah")
+ 5*2
+ 5/8
+ 
 
+def add(a,b):
+    add=a+b
+    print(add)
+    
+add(234,4560)    
+add=234+4560
+print(add)
+print("sum is", add)
+
+chocolate1= "milk"
+chocolate2= "dark"
+chocolate3= "white"
+m=5
+n=6
+o=8
+    
+print(m,chocolate1,"chocolate", n,chocolate2,"chocolate", o,chocolate3,"chocolate")
+
+import math
+dir(math)
+
+math.factorial(7)
+math.sqrt(350)
+
+math.pow(35,9)
+35**9
+math.pi
